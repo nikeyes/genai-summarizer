6daa767280f36d7950cb99c44b06eb9570751957
Jorge Castro <nikey_es@yahoo.es>
2024-10-18 15:39:49 +0200

    PoC first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# genai-summarizer
+# PoC genai-summarizer
 Resume tus videos, audios o documentos
+
+- Groq API para acceder a Whisper para las transcripciones de audio a texto
+- Amazon Bedrock como backend para acceder a LLMs (Claude) 
+- ffmpeg para comprimir el audio antes de enviarlo a Groq
---
+++ src/bedrock_client.py
@@ -0,0 +1,57 @@
+import json
+import boto3
+import os
+
+
+class BedrockClient:
+    client: boto3.client = None
+
+    def __init__(self):
+        unicron_notebooks = os.getenv('UNICRON_NOTEBOOKS')
+
+        if unicron_notebooks is None:
+            boto3.setup_default_session(profile_name='genai-dev')
+
+        aws_region_paris = 'eu-west-3'
+        aws_region_frankfurt = 'eu-central-1'
+
+        self.client = boto3.client(
+            service_name='bedrock-runtime',
+            region_name=aws_region_frankfurt,
+        )
+
+    def invoke_model(self, model_id: str, system_prompt: str, user_prompt: str):
+        body = json.dumps(
+            {
+                "system": system_prompt,
+                "anthropic_version": "bedrock-2023-05-31",
+                "max_tokens": 2048,
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": [{"type": "text", "text": user_prompt}],
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "",
+                    },
+                ],
+                "temperature": 0,
+                "top_k": 250,
+                "top_p": 0.999,
+                "stop_sequences": ["\n\nHuman:", "\n\nAssistant", "</function_calls>"],
+            }
+        )
+
+        accept = 'application/json'
+        content_type = 'application/json'
+
+        response = self.client.invoke_model(
+            modelId=model_id,
+            body=body,
+            accept=accept,
+            contentType=content_type,
+        )
+
+        completion = json.loads(response.get("body").read())
+        return completion
